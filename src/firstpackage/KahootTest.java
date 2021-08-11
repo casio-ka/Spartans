31fe88788bf15d9e83bd7ae81c3089833f55ac33
Altynai-Bai <harmoni5413@gmail.com>
2021-08-11 12:05:30 -0500

    added first comment1

--- src/firstpackage/KahootTest.java
+++ src/firstpackage/KahootTest.java
@@ -3,6 +3,6 @@ package firstpackage;
 public class KahootTest {
 
     public static void main(String[] args) {
-        System.out.println("Altynai here too");
+        System.out.println(" Altynai here too");
     }
 }
