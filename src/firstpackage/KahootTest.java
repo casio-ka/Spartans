07360d6747a2f8a3f5fd16cbb8af895828b9da56
LoganBSanders <75134563+LoganBSanders@users.noreply.github.com>
2021-08-11 12:02:18 -0500

    1st commit from Burak

--- src/firstpackage/KahootTest.java
+++ src/firstpackage/KahootTest.java
@@ -1,4 +1,22 @@
 package firstpackage;
 
 public class KahootTest {
+
+    public String name;
+    public int age;
+    public static double money;
+
+    public KahootTest(String name) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public KahootTest(int age) {
+        this.age = age;
+    }
+
+    public KahootTest(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
